8ce08639590d635ba0f2e454f383f6f957d0ca51
Whitney <whitney.purdum@gmail.com>
2017-04-17 18:42:09 -0400

    update

--- app.rb
+++ app.rb
@@ -8,12 +8,43 @@ require_relative 'db/connection'
 # Load models
 require_relative 'models/pokemon'
 
+get '/pokemons/new' do
+  erb :"pokemons/new"
+end
+
+delete '/pokemons/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  @pokemon.destroy
+  redirect("/")
+end
+
+#base
 get '/' do
   @pokemons = Pokemon.all
   erb :"pokemons/index"
 end
 
+#show selected
 get '/pokemons/:id' do
   @pokemon = Pokemon.find(params[:id])
   erb :"pokemons/show"
+end
+
+#post request for create
+post '/pokemons' do
+  @pokemon = Pokemon.create(params[:pokemon])
+  redirect "/pokemons/#{@pokemon.id}"
+end
+
+## To change
+
+get "/pokemons/:id/edit" do
+  @pokemon = Pokemon.find(params[:id])
+  erb(:"pokemons/edit")
+end
+
+put '/pokemons/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  @pokemon.update(params[:pokemon])
+  redirect("/pokemons/#{@pokemon.id}")
 end
