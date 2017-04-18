2e18d8ba962fd11040516117fa1ab4cdb4ef4d43
Whitney <whitney.purdum@gmail.com>
2017-04-18 09:01:50 -0400

    update

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,12 @@ create table pokemons (
   cp INTEGER,
   img_url VARCHAR
 );
+
+create table trainers (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR,
+  level INTEGER,
+  img_url VARCHAR,
+  pokemon_id INTEGER
+
+);
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,13 @@ Pokemon.create(name: "Clefairy", cp: rand(800), poke_type: "fairy", img_url: "ht
 Pokemon.create(name: "Nidoking", cp: rand(800), poke_type: "poison ground", img_url: "https://img.pokemondb.net/artwork/nidoking.jpg")
 Pokemon.create(name: "Gengar", cp: rand(800), poke_type: "ghost poison", img_url: "https://img.pokemondb.net/artwork/gengar.jpg")
 Pokemon.create(name: "Blastoise", cp: rand(800), poke_type: "water", img_url: "https://img.pokemondb.net/artwork/blastoise.jpg")
+
+Trainer.create(name: "Moon", level: 4, pokemon_id:  )http://vignette2.wikia.nocookie.net/pokemon/images/1/19/Female_pokemon-sun-moon-characters.png/revision/latest?cb=20161017204249
+Trainer.create(name: "Gladion", level: 1, pokemon_id:)http://cdn.bulbagarden.net/upload/thumb/2/27/Sun_Moon_Gladion.png/150px-Sun_Moon_Gladion.png
+Trainer.create(name: "Mallow", level: 7, pokemon_id:)http://cdn.bulbagarden.net/upload/thumb/3/30/Sun_Moon_Mallow.png/200px-Sun_Moon_Mallow.png
+
+#id SERIAL PRIMARY KEY,
+#name VARCHAR,
+#level INTEGER,
+#img_url VARCHAR,
+#pokemon_id INTEGER
